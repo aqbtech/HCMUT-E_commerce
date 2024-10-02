a72936e2158e9a3cbd7e94e4ef550d09e013f4ff
Tu Van Nguyen Anh Quan <tvnaquan@gmail.com>
2024-10-02 23:56:11 +0700

    final jwt with security

--- Backend/src/main/java/com/se/backend/config/CustomJwtGrantedAuthoritiesConverter.java
+++ Backend/src/main/java/com/se/backend/config/CustomJwtGrantedAuthoritiesConverter.java
@@ -4,28 +4,27 @@ import org.springframework.core.convert.converter.Converter;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.util.Assert;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.List;
 import java.util.stream.Collectors;
 
 public class CustomJwtGrantedAuthoritiesConverter implements Converter<Jwt, Collection<GrantedAuthority>> {
 
 	@Override
 	public Collection<GrantedAuthority> convert(Jwt jwt) {
-		Collection<GrantedAuthority> authorities = jwt.getClaimAsStringList("scope")
-				.stream()
-				.map(SimpleGrantedAuthority::new)
-				.collect(Collectors.toList());
-
-		Collection<GrantedAuthority> customAuthorities = jwt.getClaimAsStringList("custom_claim")
-				.stream()
-				.map(SimpleGrantedAuthority::new)
-				.collect(Collectors.toList());
-
+		List<String> authorityList = jwt.getClaimAsStringList("authorities");
+		Collection<GrantedAuthority> authorities = new ArrayList<>();
+		Assert.notNull(authorityList, "authorityList cannot be null");
+		if (!authorityList.isEmpty()) {
+			authorities = authorityList.stream()
+					.map(SimpleGrantedAuthority::new)
+					.collect(Collectors.toList());
+		}
 		// Add hardcoded role
-		customAuthorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
-
-		authorities.addAll(customAuthorities);
+		authorities.add(new SimpleGrantedAuthority("ROLE_ADMIN"));
 		return authorities;
 	}
 }
