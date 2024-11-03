4b2569a2c9cbcf80e39b4838ff0576009100287e
jimmy <chinhtran5504@gmail.com>
2024-11-03 23:14:03 +0700

    modify commit again

--- frontend/src/fetchAPI/fetchProduct.jsx
+++ frontend/src/fetchAPI/fetchProduct.jsx
@@ -1,31 +1,13 @@
-// src/api/apiAccount.js
-import axios from 'axios';
 import { axiosClient, axiosClient2 } from '../fetchAPI/axios';
 
 
-const BASE_URL = 'http://localhost:3000/products';
 
-// Hàm lấy thông tin account từ server
-export const fetchProducts = async () => {
-  try {
-    const response = await axios.get(`${BASE_URL}`);
-    return response.data; 
-  } catch (error) {
-    console.error('Lỗi khi lấy thông tin account:', error);
-    throw error;
-  }
-};
-
-
-
-//-----------------------Mới-------------------------------
-
+//-----
 export const getAllProducts = async (api) =>{
   const res = await axiosClient.get(`/products${api}`);
   
   return res.data;
 }
-
 
 export const getProductsById = async (productId) => {
   try {
