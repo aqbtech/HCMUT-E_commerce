b6bc395db5cec7f367cf31ed073d70eefe7882c9
jimmy <chinhtran5504@gmail.com>
2024-11-11 23:25:54 +0700

    dev register and login page

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -5,11 +5,10 @@ import { toast } from 'react-toastify';
 import { SignIn } from '../fetchAPI/fetchAccount';
 import Cookies from 'js-cookie'
 
-
-
 const Login = () => {
   const {navigate} = useContext(ShopContext);
-  const [id, setId] = useState('');
+
+  const [username, setUsername] = useState('');
   const [pass, setPass] = useState('');
   const [isLoading, setIsLoading] = useState(false) //set loading cho login
 
@@ -17,27 +16,35 @@ const Login = () => {
   const onSubmitHandler = async (event) => {
     event.preventDefault();
     if(isLoading === true) return; //lúc đang call api thì không gọi nhiều lần!
-    setIsLoading(true); //lúc đang call api
 
+    if (!username || !pass) {
+      toast.error("Bạn còn sót thông tin nè!");
+      return;
+    }
+    setIsLoading(true); //lúc đang call api
+ 
     const body = { 
-      "username":id, 
+      "username":username, 
       "password":pass
     };
     
     await SignIn(body)
     .then((response) => {
       toast.success('Đăng nhập thành công!');
-      setIsLoading(false); //đã gọi xong
       const token = response.data.result.token;
-      Cookies.set('token', token) //set token cho cookies
-      Cookies.set('username', id); //set username cho cookies
+      const role = response.data.result.role;
+      token && Cookies.set('token', token) //set token cho cookies
+      Cookies.set('username', username); //set username cho cookies
+      role && Cookies.set("role", role) //set role cho phiên đăng nhập này
       navigate('/'); //chuyển hướng đến trang home
     })
-    .catch((error) => {
+    .catch((error) => { 
       console.error("Lỗi khi gọi API đăng nhập:", error);
-      setIsLoading(false);
       toast.error('Có lỗi xảy ra khi đăng nhập.');
       throw(error)
+    })
+    .finally(() => {
+      setIsLoading(false); //đã gọi xong
     })
   };
    
@@ -48,15 +55,15 @@ const Login = () => {
           <p className='prata-regular text-3xl'>Đăng Nhập</p>
           <hr className='border-none h-[1.5px] w-8 bg-gray-800' />
         </div>
-        <input type="text" className='w-full px-3 py-2 border border-gray-800' placeholder='Tài khoản' value={id} onChange={(e) => setId(e.target.value)}/>
+        <input type="text" className='w-full px-3 py-2 border border-gray-800' placeholder='Tài khoản' value={username} onChange={(e) => setUsername(e.target.value)}/>
         <input type="password" className='w-full px-3 py-2 border border-gray-800' placeholder='Mật khẩu' value={pass} onChange={(e) => setPass(e.target.value)}/>
         <div className='w-full flex justify-between text-sm mt-[-8px]'>
-          <p className='cursor-pointer hover:underline'>Quên mật khẩu?</p>
+          <Link to='/reset'><p className='cursor-pointer hover:underline'>Quên mật khẩu?</p> </Link>
           {
-            <Link to='/Regist'><p className='cursor-pointer hover:text-black hover:underline'>Đăng kí</p></Link>
+            <Link to='/regist'><p className='cursor-pointer hover:text-black hover:underline'>Đăng kí</p></Link>
           }
         </div>
-        <button onClick={(event) => onSubmitHandler(event, id, pass)} type='submit' className='bg-black text-white font-light px-8 py-2 mt-4 '>Đăng nhập</button>
+        <button onClick={(event) => onSubmitHandler(event, username, pass)} type='submit' className='bg-black text-white font-light px-8 py-2 mt-4 '>{isLoading ? '...Loading' : 'Đăng Nhập'}</button>
       </form>
 
     </div>
--- frontend/src/pages/Regist.jsx
+++ frontend/src/pages/Regist.jsx
@@ -7,14 +7,13 @@ import { toast } from 'react-toastify';
 
 const Regist = () => {
   const {navigate} = useContext(ShopContext);
-
-  //giá trị 
+ 
   const [id, setId] = useState('');
   const [pass, setPass] = useState('');
   const [confirmPass, setConfirmPass] = useState('');
   const [email, setEmail] = useState(''); 
   const [hovatendem, setHovatendem] = useState('');
-  const [ten, setTen] = useState('');
+  const [ten, setTen] = useState(''); 
   const [sdt, setSdt] = useState('');
   const [date, setDate] = useState('');
 
@@ -22,41 +21,42 @@ const Regist = () => {
  
   const onSubmitHandler = async (event) => {
     event.preventDefault();
-
-    if (pass !== confirmPass) {
-      toast.error("Mật khẩu xác nhận không khớp");
-      return;
-    }
-
-    if(id.length === 0 || id.pass === 0 || id.confirmPass === 0 || id.email === 0 || id.hovatendem === 0 || id.ten === 0 
-        ||  id.sdt === 0 ||id.date === 0 ) {
-      toast.error("Dữ liệu còn trống!");
-      return;
-    }
-
     if(isLoading === true)  return;
+    if(id.length === 0 || pass.length === 0 || confirmPass.length === 0 || email.length === 0 || hovatendem.length === 0 || ten.length === 0 
+      ||  sdt.length === 0 ||date.length === 0 ) {
+      toast.error("Bạn còn sót thông tin nè!");
+      return;
+    }
+ 
+    if (pass !== confirmPass) {
+      toast.error("Mật khẩu không khớp mất rùi :(((");
+      return;
+    }
 
-    const value = {
+   
+
+    const body = {
       "username" : id,
       "password" : pass,
+      "firstName" : hovatendem,
+      "lastName" : ten,
       "email" : email,
-      "hovatendem" : hovatendem,
-      "ten" : ten,
-      "phone" : sdt,
-      "DOB" : date
+      "DOB" : date,
+      "phone" : sdt
     }
     
-    await register(value)
+    await register(body)
     .then(() => {
       toast.success('Đăng kí thành công!')
-      setIsLoading(false);
-      navigate('/login');
+      navigate('/Login');
     })
     .catch((error) => {
       console.log("Lỗi đăng kí:", error)
-      toast.error("Lỗi đăng kí");
-      setIsLoading(false);
+      toast.error("Lỗi đăng kí, vui lòng thử lại!");
       throw error
+    })
+    .finally(() => {
+      setIsLoading(false); //đã gọi API xong
     })
   }  
 
@@ -73,7 +73,7 @@ const Regist = () => {
         <input type="email" className='w-full px-3 py-2 border border-gray-800' placeholder='E-mail' value={email} onChange={(e) => setEmail(e.target.value)}/>
         <div className='flex gap-3'>
             <input className='border border-gray-300  rounded py-1.5 px-3.5 w-full' type="text" placeholder='Họ và tên đệm' value={hovatendem} onChange={(e) => setHovatendem(e.target.value)}/>
-            <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Tên'value={ten} onChange={(e) => setTen(e.target.value)}/>
+            <input className='border border-gray-300 rounded py-1.5 px-3.5 w-full' type="text" placeholder='Tên' value={ten} onChange={(e) => setTen(e.target.value)}/>
         </div>
         <input type="text" className='w-full px-3 py-2 border border-gray-800' placeholder='Số điện thoại' value={sdt} onChange={(e) => setSdt(e.target.value)}/>
         <input type="date" className='w-full px-3 py-2 border border-gray-800' placeholder='Ngày-tháng-năm sinh' value={date} onChange={(e) => setDate(e.target.value)}/>
@@ -90,4 +90,5 @@ const Regist = () => {
   )
 }
  
-export default Regist
+export default Regist 
+ 
