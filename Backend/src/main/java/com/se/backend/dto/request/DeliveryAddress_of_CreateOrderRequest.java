b2a62e86409aa3e92a3c24aebc5f9bdb7f15d99e
quydoan0316 <quydoan0316@gmail.com>
2024-11-12 22:01:38 +0700

    type: fix fix the function of create order for buyer change request and response, logic in code

---
+++ Backend/src/main/java/com/se/backend/dto/request/DeliveryAddress_of_CreateOrderRequest.java
@@ -0,0 +1,11 @@
+package com.se.backend.dto.request;
+
+public class DeliveryAddress_of_CreateOrderRequest {
+    private String id;
+    private String name;
+    private String phone;
+    private String province;
+    private String district;
+    private String ward;
+    private String Detail;
+}
