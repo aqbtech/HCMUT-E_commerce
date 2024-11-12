b4d77c7506e6ecf74f869b7e98c06aa96e580066
jimmy <chinhtran5504@gmail.com>
2024-11-12 20:06:26 +0700

    modify home page

--- frontend/src/components/homePage/LatestProduct.jsx
+++ frontend/src/components/homePage/LatestProduct.jsx
@@ -4,18 +4,21 @@ import ProductItem from '../ProductItem';
 const LatestProduct = ({data}) => {
  
 
-  return (
+  return ( 
     <div className='my-10'>
-        <div className='text-center py-8 text-3xl'>
+        {/* <div className='text-center py-8 text-3xl'>
             <Title text1={'HÀNG'} text2={'MỚI'}/>
-            <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
-            Nếu bạn đang tìm kiếm một trang web để mua và bán hàng trực tuyến thì ATOM.vn là một sự lựa chọn tuyệt vời dành cho bạn </p>
+           
+        </div> */}
+        <div className="text-left py-8 text-xl">
+          <Title text1={'SẢN'} text2={'PHẨM'} />
+          <p className=' text-xs sm:text-sm md:text-base text-gray-600'>
+           ATOM.vn là một sự lựa chọn tuyệt vời dành cho bạn </p>
         </div>
-
         {/*Rendering Product */}
-        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 gap-y-6 '>
+        <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-5 '>
           {
-              data.map((item, index) => (
+              data?.map((item, index) => (
                 <ProductItem key={index} id={item.id} image={item.image} name={item.name} price={item.price}/>
               ))
           }
@@ -25,3 +28,4 @@ const LatestProduct = ({data}) => {
 }
 
 export default LatestProduct 
+ 
