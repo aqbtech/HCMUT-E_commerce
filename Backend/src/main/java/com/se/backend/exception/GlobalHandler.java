dde48c8ecc4261ce021dbbccf6bfbed69a14aefa
Tu Van Nguyen Anh Quan <tvnaquan@gmail.com>
2024-11-21 23:02:57 +0700

    handler illegal argument exception cart

--- Backend/src/main/java/com/se/backend/exception/GlobalHandler.java
+++ Backend/src/main/java/com/se/backend/exception/GlobalHandler.java
@@ -1,6 +1,7 @@
 package com.se.backend.exception;
 
 import com.se.backend.dto.response.ResponseAPITemplate;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -13,6 +14,16 @@ public class GlobalHandler {
 		ResponseEntity<ResponseAPITemplate<String>> res = ResponseEntity.status(e.getErrorCode().getHttpStatusCode())
 				.body(ResponseAPITemplate.<String>builder()
 						.code(e.getErrorCode().getCode())
+						.message(e.getMessage())
+						.result(null)
+						.build());
+		return res;
+	}
+	@ExceptionHandler(IllegalArgumentException.class)
+	public ResponseEntity<ResponseAPITemplate<String>> handleWebServerException(IllegalArgumentException e) {
+		ResponseEntity<ResponseAPITemplate<String>> res = ResponseEntity.status(HttpStatus.BAD_REQUEST)
+				.body(ResponseAPITemplate.<String>builder()
+						.code(400)
 						.message(e.getMessage())
 						.result(null)
 						.build());
