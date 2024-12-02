f2b47ba57dcb45d344f98f29353340cc5b40226c
jimmy <chinhtran5504@gmail.com>
2024-12-03 01:47:01 +0700

    state cancelled

--- frontend/src/pages/Orders.jsx
+++ frontend/src/pages/Orders.jsx
@@ -135,7 +135,7 @@ const Orders = () => {
                       ? "bg-yellow-100 text-yellow-600" 
                       : order.deliveryState === "COMPLETED"
                       ? "bg-green-100 text-green-600" // Đã giao
-                      : order.deliveryState === "CANCEL"
+                      : order.deliveryState === "CANCELLED"
                       ? "bg-red-100 text-red-600" // Đã hủy
                       : "" // Nếu không có trạng thái nào
                   }`}
@@ -148,7 +148,7 @@ const Orders = () => {
                     ? "Đang giao"
                     : order.deliveryState === "COMPLETED"
                     ? "Đã giao"
-                    : order.deliveryState === "CANCEL"
+                    : order.deliveryState === "CANCELLED"
                     ? "Đã hủy"
                     : ""}
                 </span>
