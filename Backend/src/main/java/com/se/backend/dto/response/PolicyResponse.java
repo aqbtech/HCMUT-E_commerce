ffabd7f342ed53ba0346840f6bc229b8138206b3
quydoan0316 <quydoan0316@gmail.com>
2024-12-07 02:22:05 +0700

    Hotfix in Policy: add id for policy response, add target shop/category for response

--- Backend/src/main/java/com/se/backend/dto/response/PolicyResponse.java
+++ Backend/src/main/java/com/se/backend/dto/response/PolicyResponse.java
@@ -4,6 +4,7 @@ import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDate;
+import java.util.List;
 
 @Data
 @Builder
@@ -13,12 +14,14 @@ public class PolicyResponse {
 //    @Data
 //    @Builder
 //    public static class ShopPolicyResponse{
+        private Long policyId;
         private LocalDate apply_date;
         private String policy_description;
         private String policy_name;
         private LocalDate release_date;
         private Double sale;
         private Long count;
+        private List<String> target;
 //    }
 //    @Data
 //    @Builder
